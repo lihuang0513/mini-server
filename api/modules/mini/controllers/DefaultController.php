afb5b1a603583c38f827a638418eabfe991fc5e2
李煌 <517306208@qq.com>
2020-02-27 23:08:03 +0800

    first commit

---
+++ api/modules/mini/controllers/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace api\modules\mini\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Default controller for the `mini` module
+ */
+class DefaultController extends Controller
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+		echo 'this is mini interface huhu1';
+    }
+}
